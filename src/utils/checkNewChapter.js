b60e6cc33552d0ebc72c32ccb3204f42c2da595d
wlitkopa <wlitkopa@o2.pl>
2023-12-14 14:48:48 +0100

    Solved problem with getting last chapter number

--- src/utils/checkNewChapter.js
+++ src/utils/checkNewChapter.js
@@ -29,12 +29,62 @@ module.exports = async (dbComic) => {
     const dom = new jsdom.JSDOM(comicWebHtml);
     console.log(`dom: ${dom}`)
     console.log(`dom.window.document: ${dom.window.document}`);
-    var chapterList = dom.window.document.querySelectorAll("h1");
-    console.log(`${chapterList}`);
+    var chapterList = dom.window.document.getElementsByClassName("scroll-sm");
+    console.log(`chapterList: ${chapterList}`);
+    console.log(`chapterList[0] :${chapterList[0]}`);
+    var firstChild = chapterList[0].firstChild;
+    var itemChapterList = chapterList[0].getElementsByClassName('item');
+    console.log(`itemChapterList.length: ${itemChapterList.length}`);
+    // console.log(`liChapterList.length: ${liChapterList}`);
+    var dataNumbers = dom.window.document.querySelector('[data-number]');
+    console.log(`dataNumbers: ${dataNumbers}`)
+    console.log(`dataNumbers.value: ${dataNumbers.value}`)
+    console.log(`dataNumbers.nodeValue: ${dataNumbers.nodeValue}`)
+    console.log(`dataNumbers.nodeName: ${dataNumbers.nodeName}`)
+    console.log(`dataNumbers.nodeType: ${dataNumbers.nodeType}`)
+    console.log(`dataNumbers.getAttributeNames(): ${dataNumbers.getAttributeNames()}`)
 
-    for (let chapter in chapterList) {
+    // ROZWIĄZANIE!!!!!!!!! JEST TUUUUUTAAAAAAAAAJ::::::
+    console.log(`dataNumbers.getAttribute('data-number'): ${dataNumbers.getAttribute('data-number')}`)
+
+    console.log(`dataNumbers.tagName: ${dataNumbers.tagName}`)
+    console.log(`dataNumbers.outerHTML: ${dataNumbers.outerHTML}`)
+    console.log(`dataNumbers: ${dataNumbers}`)
+
+
+    for (let chapter in dataNumbers) {
         console.log(`chapter: ${chapter}`);
     }
+
+    // console.log(`chapterList[0].outerHTML :${chapterList[0].outerHTML}`);
+    console.log(`chapterList[0].hasChildNodes(): ${chapterList[0].hasChildNodes()}`);    
+    console.log(`dataNumbers.outerHTML: ${dataNumbers.outerHTML}`)
+
+    console.log(`chapterList[0].firstChild: ${chapterList[0].firstChild}`);
+    console.log(`chapterList[0].ELEMENT_NODE: ${chapterList[0].ELEMENT_NODE}`);
+    console.log(`chapterList[0].ATTRIBUTE_NODE: ${chapterList[0].ATTRIBUTE_NODE}`);
+    console.log(`chapterList[0].TEXT_NODE: ${chapterList[0].TEXT_NODE}`);
+    console.log(`chapterList[0].CDATA_SECTION_NODE: ${chapterList[0].CDATA_SECTION_NODE}`);
+    console.log(`chapterList[0].getElementsByTagName('li'): ${chapterList[0].getElementsByTagName('li')}`);
+
+
+    console.log(`chapterList[0].firstChild: ${chapterList[0].firstChild}`);
+    console.log(`chapterList[0].firstChild.data: ${chapterList[0].firstChild.data}`);
+    console.log(`chapterList[0].firstChild.wholeText: ${chapterList[0].firstChild.wholeText}`);
+    console.log(`chapterList[0].firstChild.length: ${chapterList[0].firstChild.length}`);
+    console.log(`chapterList[0].firstChild.textContent: ${chapterList[0].firstChild.textContent}`);
+    // console.log(`chapterList[0].firstChild.getAttribute('data-number'): ${chapterList[0].firstChild.getAttribute('data-number')}`);
+
+
+
+
+
+
+
+
+
+
+    var lastChapter = chapterList[0]
 
     // Regex parser
     // var chapters = comicWebHtml.match(chaptersReg) || 0;
